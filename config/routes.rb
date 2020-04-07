961ade346ec61a65f46b9f3c8bd19ccab532edf8
Kasperovich <zhenkasp@mail.ru>
2020-04-07 15:55:10 +0300

    add about, add styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  get 'about' => 'about#index'
+
+  resources :users, only: [:show, :update, :edit]
+  resources :posts
+  #do
+  #resources :comments, only: [:create, :destroy]
+
+  root 'posts#index'
 end
