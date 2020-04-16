6f1c44a46a0cced8d1b34d163494829379792e50
Kasperovich <zhenkasp@mail.ru>
2020-04-16 19:09:08 +0300

    add posts

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+
+
+  #validates :title, presence: true, length: { in: 2..25 } ,uniqueness: true
+  #validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+end
