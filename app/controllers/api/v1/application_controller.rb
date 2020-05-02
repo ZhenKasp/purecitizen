939fca48fcbf3fb7bfb999160d24ac48a9270292
Kasperovich <zhenkasp@mail.ru>
2020-05-02 16:01:07 +0300

    api v1

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -4,16 +4,16 @@ module Api
       protect_from_forgery with: :null_session
       before_action :authenticate
 
-      def current_user
-        @current_user ||= authenticate
-      end
-
       private
 
       def authenticate
-        authenticate_or_request_with_http_token do |token, _options|
-          User.find_by(token: token)
+        authenticate_or_request_with_http_token do |token, options|
+          @current_user = User.find_by(token: token)
         end
+
+      def  current_user
+        @current_user ||= authenticate
+      end
       end
     end
   end
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,11 +1,12 @@
 module Api
   module V1
     class PostsController < Api::V1::ApplicationController
-      skip_before_action :authenticate
+      #skip_before_action :authenticate
 
       def index
-          @post = Post.all.order(:title)
-        render json: { posts: @post }
+
+        @post = Post.all.order(:title)
+        render json: { post: @post }
       end
 
       def show
